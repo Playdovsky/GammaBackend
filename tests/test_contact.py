5eb5edcd0d2a016b4666c23e5366b452afbcf147
Playdovsky <144317216+Playdovsky@users.noreply.github.com>
2026-07-17 16:40:58 +0200

    small changes & additional test for /contact endpoint

--- tests/test_contact.py
+++ tests/test_contact.py
@@ -20,12 +20,18 @@ payload3 = {
 
 payload4 = {
     "name": "Carlos",
-    "email": "carlos_fontanciones@gmail",
+    "email": "carlos_fontanciones@gmail.com",
 }
 
 payload5 = {
     "name": "                          ",
-    "email": "carlos_fontanciones@gmail",
+    "email": "carlos_fontanciones@gmail.com",
+    "message": "This is a test message"
+}
+
+payload6 = {
+    "name": "david",
+    "email": "david-horse/man@protonmail.com",
     "message": "This is a test message"
 }
 
@@ -39,18 +45,28 @@ def test_contact_success(client: TestClient):
     assert data["message"] == "This is a test message"
     assert data["archived"] == False
 
-def test_contact_fail(client: TestClient):
+def test_contact_invalid_email(client: TestClient):
     response = client.post("/api/contact", json=payload2)
     assert response.status_code == 400
 
-def test_contact_fail2(client: TestClient):
+def test_contact_invalid_email2(client: TestClient):
     response = client.post("/api/contact", json=payload3)
     assert response.status_code == 400
     
-def test_contact_fail3(client: TestClient):
+def test_contact_invalid_no_message(client: TestClient):
     response = client.post("/api/contact", json=payload4)
     assert response.status_code == 422
 
-def test_contact_fail4(client: TestClient):
+def test_contact_invalid_whitespace_name(client: TestClient):
     response = client.post("/api/contact", json=payload5)
     assert response.status_code == 400
+
+def test_contact_success_different_characters(client: TestClient):
+    response = client.post("/api/contact", json=payload6)
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data["name"] == "david"
+    assert data["email"] == "david-horse/man@protonmail.com"
+    assert data["message"] == "This is a test message"
+    assert data["archived"] == False
